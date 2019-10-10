4eaee9a977ff12bd8be00a92830d2815916a70f9
Melanie Freshness <david.alex.thompson@gmail.com>
2019-10-10 21:17:50 +0100

    db changed to postgres

--- hiSky/settings.py
+++ hiSky/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'hiSky.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hiSkydb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST':'127.0.0.1',
+        'PORT': '5432'
     }
 }
 
